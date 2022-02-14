02c9c5b58dbeece3d77f654f77de492fdfd76713
Mike Pashchenko <m.pashchenko.92@gmail.com>
2022-02-14 10:30:14 +0300

    fix full_title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
     unless page_title.empty?
-      page_title + ' | ' + base_title
+      base_title = page_title + ' | ' + base_title
     end
+    base_title
   end
 end
