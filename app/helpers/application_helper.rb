14502e007a484106fc69040de99a57a14eb89ed9
Mike Pashchenko <m.pashchenko.92@gmail.com>
2022-02-14 13:25:27 +0300

    add email format validation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+
+  VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
     unless page_title.empty?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  #include ApplicationHelper
+
   validates :name, presence: true, length: { maximum: 50}
-  validates :email, presence: true, length: { in: 3..255 }
+  validates :email,
+    presence: true, length: { in: 3..255 },
+    format: { with: ApplicationHelper::VALID_EMAIL_REGEXP },
+    uniqueness: { case_sensitive: false }
 end
