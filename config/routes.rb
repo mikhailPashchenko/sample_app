a201c3029cc1218ff0bffc716f207925a0a25907
Mike Pashchenko <m.pashchenko.92@gmail.com>
2022-02-14 10:43:05 +0300

    users/new: route, controller, test, view

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get '/about',to: 'static_pages#about'
   get '/contacts', to: 'static_pages#contacts'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'users/new', to: 'users#new'
 end
