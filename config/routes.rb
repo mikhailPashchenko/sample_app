795210afa0d64d6a100fc8e8bcb29bc338e201d1
Mike Pashchenko <m.pashchenko.92@gmail.com>
2022-02-14 10:55:06 +0300

    change signup url and add tests

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get '/contacts', to: 'static_pages#contacts'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'users/new', to: 'users#new'
+  get '/signup', to: 'users#new'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,30 +4,30 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get home_path
     assert_response :success
-    assert_select 'title', "Home | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', full_title('Home')
   end
 
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select 'title', "Help | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', full_title('Help')
   end
 
   test "shoulg get about" do
     get about_path
     assert_response :success
-    assert_select 'title', "About | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', full_title('About')
   end
 
   test "should get contacts" do
     get contacts_path
     assert_response :success
-    assert_select 'title', "Contacts | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', full_title('Contacts')
   end
 
   test "should get root" do
     get root_path
     assert_response :success
-    assert_select 'title', "Home | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', full_title('Home')
   end
 end
