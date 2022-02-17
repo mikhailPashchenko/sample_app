7940f9083d392cf439c4a19f65307639b73e2825
Mike Pashchenko <m.pashchenko.92@gmail.com>
2022-02-17 11:09:52 +0300

    add flash to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      render :new
+      flash[:danger] = "Try again!"
+      render :new, status: :unprocessable_entity
     end
   end
 
