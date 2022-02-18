9934dbdeef8276c3232bced96f08cab0cc0f7eab
Mike Pashchenko <m.pashchenko.92@gmail.com>
2022-02-18 20:56:54 +0300

    add session controler, login form and create session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
+
   def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in(user)
+      flash[:success] = 'You are logged in!'
+      return redirect_to user
+    elsif user.nil?
+      flash.now[:danger] = 'Email not found!'
+    else
+      flash.now[:danger] = 'Incorrect password!'
+    end
+    render :new
   end
 end
