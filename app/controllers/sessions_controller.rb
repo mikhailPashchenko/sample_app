474177467eb5cb33f2ea3eebc20fdf538a5a81e7
Mike Pashchenko <m.pashchenko.92@gmail.com>
2022-02-18 22:14:34 +0300

    check user logged_in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
   include SessionsHelper
 
+  before_action :set_current_user
+
   def new
+    if @current_user
+      flash[:warning] = 'You are already logged in!'
+      redirect_to @current_user
+    end
   end
 
   def create
+    if @current_user
+      return redirect_to @current_user
+    end
+
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       reset_session
@@ -17,5 +27,15 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'Incorrect password!'
     end
     render :new
+  end
+
+  def destroy
+    reset_session
+    @current_user = nil
+  end
+
+  private
+  def set_current_user
+    @current_user ||= current_user
   end
 end
