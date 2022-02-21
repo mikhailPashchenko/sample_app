4051a9cd8d5573e0c31b79526b6713ba575be553
Mike Pashchenko <m.pashchenko.92@gmail.com>
2022-02-21 20:30:34 +0300

    edit form and controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   include SessionsHelper
 
+  before_action :find_user, except: [:new, :create]
+
   def new
     @user = User.new
   end
@@ -20,14 +22,28 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id])
     if @user.nil?
       redirect_to root_path, flash: { warning: 'User not found!' }
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
     end
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def find_user
+    @user = User.find_by(id: params[:id])
   end
 end
