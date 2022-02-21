a48731be11811a5c1852de20d59c5781d65f7356
Mike Pashchenko <m.pashchenko.92@gmail.com>
2022-02-21 12:39:42 +0300

    tests for double logout

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,6 +4,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:one)
+    @password = 'password'
+  end
+
+  def login_without_redirect
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: @password } }
+  end
+
+  def logout_without_redirect
+    login_without_redirect
+    follow_redirect!
+    delete logout_path
   end
 
   test "login with invalid params" do
@@ -17,29 +30,35 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "login with valid params" do
-    get login_path
-    post login_path, params: { session: { email: @user.email,
-                                          password: 'password' } }
+    login_without_redirect
     assert_redirected_to @user
     follow_redirect!
     assert_template :show
-    assert_select "a[href=?]", login_url, count: 0
+    assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
     assert is_logged_in?
   end
 
   test "user log out" do
-    get login_path
-    post login_path, params: { session: { email: @user.email,
-                                          password: 'password' } }
-    follow_redirect!
-    delete logout_path
+    logout_without_redirect
     assert_not is_logged_in?
     assert_redirected_to root_path
     follow_redirect!
-    assert_select "a[href=?]", login_url
-    assert_select "a[href=?]", logout_url, count: 0
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "user log out 2 times" do
+    logout_without_redirect
+    follow_redirect!
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
+    follow_redirect!
+
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
