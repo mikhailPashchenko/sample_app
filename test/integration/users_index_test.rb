d1658ee9e7d272fdfec81e46dbe44bd781f6575e
Mike Pashchenko <m.pashchenko.92@gmail.com>
2022-02-23 15:04:08 +0300

    users index tests for admin and non-admin

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -7,28 +7,46 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   end
 
   def setup
-    @user = users(:mike_admin)
+    @admin = users(:mike_admin)
+    @client = users(:client)
     @last_user = User.last
-    login_as(@user)
-    get users_path
   end
 
   test "index should have pagination with 2 pages" do
+    login_as(@client)
+    get users_path
     assert_select "div.pagination", count: 2
     assert_select "a[href=?]", "#{users_path}?page=2"
   end
 
   test "index should have first page with users" do
+    login_as(@client)
+    get users_path
     User.paginate(page: 1).each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
     end
   end
 
   test "click next page should redirect to page 2" do
+    login_as(@client)
+    get users_path
     assert_select "a[rel=next]", href: "#{users_path}?page=2"
     get "#{users_path}?page=2"
     User.paginate(page: 2).each do |user|
       assert_select "a[href=?]", user_path(user), text: user.name
     end
+  end
+
+  test "non-admin should not see delete links" do
+    login_as(@client)
+    get users_path
+    assert_select "a", text: 'delete', count: 0
+  end
+
+  test "admin should see delete links except for himself" do
+    login_as(@admin)
+    get users_path
+    assert_select "a", text: 'delete', count: 29
+    assert_select "a[href=?]", user_path(@admin), text: "delete", count: 0
   end
 end
